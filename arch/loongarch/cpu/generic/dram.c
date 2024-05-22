6ae6778ee441228c63bdbf32f4a3c036df104477
Jiaxun Yang <jiaxun.yang@flygoat.com>
2024-05-22 16:34:54 +0100

    LoongArch: Generic platform type
    Provide a generic platform with some common routines expected to be
    implemented at CPU level.
    
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Signed-off-by: Yao Zi <me@ziyao.cc>

---
+++ arch/loongarch/cpu/generic/dram.c
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2024 Jiaxun Yang <jiaxun.yang@flygoat.com>
+ */
+
+#include <fdtdec.h>
+
+int dram_init(void)
+{
+	return fdtdec_setup_mem_size_base_lowest();
+}
+
+int dram_init_banksize(void)
+{
+	return fdtdec_setup_memory_banksize();
+}
