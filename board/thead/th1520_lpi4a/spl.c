0b411d168204c53a1076552002b44812929665cb
Yao Zi <ziyao@disroot.org>
2025-02-17 10:28:32 +0000

    board: thead: lpi4a: implement spl_boot_device()

--- board/thead/th1520_lpi4a/spl.c
+++ board/thead/th1520_lpi4a/spl.c
@@ -3,9 +3,27 @@
  * Copyright (c) 2025, Yao Zi <ziyao@disroot.org>
  */
 
-#include <cpu_func.h>
+#include <asm/io.h>
+#include <asm/spl.h>
+#include <asm/arch/sysctl_regs.h>
 
 u32 spl_boot_device(void)
 {
-	return 0;
+	switch (readl(TH1520_SYSCTL_BASE + TH1520_SYSCTL_BOOT_SEL) & 0x7) {
+	case 0:
+	case 1:
+	case 2:
+	case 3:
+		return BOOT_DEVICE_BOOTROM;
+	case 4:
+		return BOOT_DEVICE_MMC1;
+	case 5:
+		return BOOT_DEVICE_MMC2;
+	case 6:
+		return BOOT_DEVICE_NAND;
+	case 7:
+		return BOOT_DEVICE_SPI;
+	}
+
+	unreachable();
 }
