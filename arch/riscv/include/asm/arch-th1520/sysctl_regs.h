5e8714bec4b49c2e1e5d60e2d6515566be2b248f
Yao Zi <ziyao@disroot.org>
2025-02-17 10:34:05 +0000

    riscv: cpu: th1520: Add sysctl regs
    Signed-off-by: Yao Zi <ziyao@disroot.org>

---
+++ arch/riscv/include/asm/arch-th1520/sysctl_regs.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2025 Yao Zi <ziyao@disroot.org>
+ */
+#ifndef _ASM_TH1520_SYSCTL_REG_H_
+#define _ASM_TH1520_SYSCTL_REG_H_
+
+#define TH1520_SYSCTL_BASE		((void *)0xffef018000)
+#define TH1520_SYSCTL_BOOT_SEL		0x10
+
+#endif /* _ASM_TH1520_SYSCTL_REG_H_ */
