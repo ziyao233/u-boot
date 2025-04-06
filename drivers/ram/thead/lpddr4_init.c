a00b8f8d709d4188ea78bbd8b9c9ac5827231ea1
Yao Zi <ziyao@disroot.org>
2025-04-06 11:03:28 +0000

    ram: thead: obtain firmware from binman
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- drivers/ram/thead/lpddr4_init.c
+++ drivers/ram/thead/lpddr4_init.c
@@ -3,7 +3,36 @@
 #include "ddr_common_func.h"
 #include "lpddr4_init.h"
 
+#include <binman.h>
+#include <binman_sym.h>
+
 extern void lp4_phy_train1d2d(enum DDR_TYPE type, int speed, enum DDR_BITWIDTH bits);
+
+struct th1520_ddr_fw {
+	uint64_t magic;
+	uint8_t type, bitwidth, freq;
+	uint8_t reserved[8];
+	uint32_t cfgnum;
+};
+
+binman_sym_declare(ulong, ddr_fw, image_pos);
+
+static int lpddr4_load_firmware(void)
+{
+	struct th1520_ddr_fw *fw = (void*)binman_sym(ulong, ddr_fw, image_pos);
+
+	printf("%s fw = 0x%lx\n", __func__, (unsigned long)fw);
+	printf("%s: _binman_sym_magic = %lx\n, %lx\n", __func__,
+	       &_binman_sym_magic, _binman_sym_magic);
+
+	printf("Firmware information:\n");
+	printf("magic = 0x%llx, type = 0x%x, bitwidth = %d, freq = %x",
+	       fw->magic, fw->type, fw->bitwidth, fw->freq);
+
+	printf("%d configuration entires in total\n", fw->cfgnum);
+
+	return 0;
+}
 
 void lpddr4_init(enum DDR_TYPE type, int rank_num, int speed, enum DDR_BITWIDTH bits)
 { 
@@ -21,6 +50,8 @@ void lpddr4_init(enum DDR_TYPE type, int rank_num, int speed, enum DDR_BITWIDTH 
   addrmap(rank_num, bits);
 
   de_assert_other_reset_ddr();
+
+  lpddr4_load_firmware();
 
   dq_pinmux(bits); // pinmux config before training
 
